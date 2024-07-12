b1092359a0bc1d58af180606fca815898270cdd4
nnnlog <sorisem4106@naver.com>
2024-07-12 09:33:26 +0900

    Apply font correctly

--- src/App.vue
+++ src/App.vue
@@ -74,7 +74,7 @@ body {
   //  BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
   //  "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji",
   //  "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
-  font-family: "Pretendard Variable";
+  font-family: "Pretendard";
 }
 
 ::selection {
